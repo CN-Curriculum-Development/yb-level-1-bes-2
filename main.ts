8072c38094aee841f0f0534d325607f6f5383791
ssmolevitzCN <87033831+ssmolevitzCN@users.noreply.github.com>
2021-08-16 17:24:16 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -97,7 +97,7 @@ let bunny = sprites.create(img`
     . . . . c c b c c c c c . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(bunny)
-tiles.placeOnRandomTile(bunny, sprites.castle.tilePath5)
+tiles.placeOnRandomTile(bunny, assets.tile`myTile0`)
 scene.cameraFollowSprite(bunny)
 let farmer = sprites.create(img`
     . . . . . b b c c c c . . . . . 
@@ -117,7 +117,7 @@ let farmer = sprites.create(img`
     . . . . f 8 8 6 6 8 8 f . . . . 
     . . . . . f f b b f f . . . . . 
     `, SpriteKind.Enemy)
-tiles.placeOnRandomTile(farmer, sprites.castle.tilePath5)
+tiles.placeOnRandomTile(farmer, assets.tile`myTile0`)
 farmer.follow(bunny, 25)
 info.setScore(0)
 info.setLife(3)
@@ -140,7 +140,7 @@ game.onUpdateInterval(5000, function () {
         . . . . . 4 4 e . . . . . . . . 
         . . . . . . 4 e . . . . . . . . 
         `, SpriteKind.SpecialFood)
-    tiles.placeOnRandomTile(carrot, sprites.castle.tilePath5)
+    tiles.placeOnRandomTile(carrot, assets.tile`myTile0`)
 })
 game.onUpdateInterval(1000, function () {
     sprout = sprites.create(img`
@@ -159,5 +159,5 @@ game.onUpdateInterval(1000, function () {
         . . . e . . e . e . . . . . . 
         . . . . . . . . . . e . . . . 
         `, SpriteKind.Food)
-    tiles.placeOnRandomTile(sprout, sprites.castle.tilePath5)
+    tiles.placeOnRandomTile(sprout, assets.tile`myTile0`)
 })
